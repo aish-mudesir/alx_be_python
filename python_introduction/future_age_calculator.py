b7c5c90156672238e494f1c1ffb03d7ffa2d2c60
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-09-23 15:08:05 +0300

    Create future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# future_age_calculator.py
+
+# Prompt the user to input their current age
+current_age = int(input("How old are you? "))
+
+# Calculate the future age in 2050
+# Assuming the current year is 2023
+future_age = current_age + 27
+
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
